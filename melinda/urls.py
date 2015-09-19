8a3b3e4c78505670496dd69c9ee317c1de95703e
Jacob Wan <TeacherJacob@yahoo.com>
2015-09-19 12:33:31 -0600

    added urls.py

--- melinda/urls.py
+++ melinda/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.models import User
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from rest_framework import routers, serializers, viewsets
 
 from searchapp import views
@@ -20,4 +20,5 @@ urlpatterns = patterns('',
     url(r'^api-auth/',include('rest_framework.urls', namespace='rest_framework')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+) 
+urlpatterns += staticfiles_urlpatterns()
